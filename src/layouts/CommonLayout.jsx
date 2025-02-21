09708c22de13fadff0fd0d2d18b49e7dfd1c2cba
Rafi5734 <kamrulhasun5734@gmail.com>
2025-02-21 11:50:17 +0600

    pre-loader added for after dom loading

--- src/layouts/CommonLayout.jsx
+++ src/layouts/CommonLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Sidebar from "../components/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
 import NavbarMain from "../components/navbarMain/NavbarMain";
@@ -6,26 +6,44 @@ import { useWindowSize } from "@uidotdev/usehooks";
 
 export default function CommonLayout() {
   const size = useWindowSize();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1500);
+
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
-    <div className="flex">
-      {/* Sidebar */}
-      <Sidebar size={size} />
+    <>
+      {loading && (
+        <div className="fixed inset-0 flex items-center justify-center bg-white transition-opacity duration-500">
+          <div className="w-12 h-12 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
+        </div>
+      )}
 
-      {/* Main Content */}
-      <div
-        className={`flex flex-col ${
-          size?.width < 640 ? "w-full" : "ml-64 w-[calc(100%-16rem)]"
-        }`}
-      >
-        {/* Navbar */}
-        <NavbarMain size={size} />
+      {/* Main Layout */}
+      <div className={`flex transition-opacity duration-500 ${loading ? "opacity-0" : "opacity-100"}`}>
+        {/* Sidebar */}
+        <Sidebar size={size} />
 
-        {/* Page Content */}
-        <main>
-          <Outlet />
-        </main>
+        {/* Main Content */}
+        <div
+          className={`flex flex-col ${
+            size?.width < 640 ? "w-full" : "ml-64 w-[calc(100%-16rem)]"
+          }`}
+        >
+          {/* Navbar */}
+          <NavbarMain size={size} />
+
+          {/* Page Content */}
+          <main>
+            <Outlet />
+          </main>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
